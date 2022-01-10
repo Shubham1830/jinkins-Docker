e2f06b4095dd15ea82424960f58600b85456f2b3
Shubham <shubham@.com>
2022-01-10 21:48:36 +0530

    Add Files

---
+++ src/main/java/com/exampe/jinkins_docker/App.java
@@ -0,0 +1,22 @@
+package com.exampe.jinkins_docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    	
+	public static boolean cheackIfInputIsAnEvenNumber(int number)
+	{
+		return number % 2 == 0;
+	}
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(cheackIfInputIsAnEvenNumber(122));
+
+	}
+}
---
+++ src/test/java/com/exampe/jinkins_docker/AppTest.java
@@ -0,0 +1,18 @@
+package com.exampe.jinkins_docker;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testInputIsEven(){
+        assertTrue(App.cheackIfInputIsAnEvenNumber(22)); // Assertion
+}}
